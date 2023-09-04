53844c6e8bb6b67cf60414e09c01ddb653bdcd6d
Manel Boumansour <manel.boumansour.etu@univ-lille.fr>
2023-09-04 21:33:35 +0200

    MAJ bingo

--- src/main/Bingo/bingo.java
+++ src/main/Bingo/bingo.java
@@ -1,11 +1,12 @@
+import java.util.ArrayList;
 import java.util.Random;
 
 public class bingo {
-    int[] grille1 = new int[9];
-    int[] grille2 = new int[9];
-    int[] grille3 = new int[9];
-    int[] grille4 = new int[9];
-    int[] grille5 = new int[9];
+    ArrayList grille1 = new ArrayList<>();
+    ArrayList grille2 = new ArrayList<>(null);
+    ArrayList grille3 = new ArrayList<>(null);
+    ArrayList grille4 = new ArrayList<>(null);
+    ArrayList grille5 = new ArrayList<>(null);
     boolean[] trouve1 = new boolean[9];
     boolean[] trouve2 = new boolean[9];
     boolean[] trouve3 = new boolean[9];
@@ -18,20 +19,50 @@ public class bingo {
         //création des cinqs grilles
         for (int i=0; i<9; i++){
             r.nextInt(45);
-            //if(r not in grille1)
-            grille1[i] =  r.nextInt(45);
+            if(!grille1.contains(r)){
+                grille1.add(r.nextInt(45));
+            }else{
+                while(!grille1.contains(r)){
+                    r.nextInt(45);
+                    grille1.add(r.nextInt(45));
+                }
+            }
             r.nextInt(45);
-            //if(r not in grille2)
-            grille2[i] =  r.nextInt(45);
+            if(!grille2.contains(r)){
+                grille2.add(r.nextInt(45));
+            }else{
+                while(!grille2.contains(r)){
+                    r.nextInt(45);
+                    grille2.add(r.nextInt(45));
+                }
+            }
             r.nextInt(45);
-            //if(r not in grille3)
-            grille3[i] =  r.nextInt(45);
+            if(!grille3.contains(r)){
+                grille3.add(r.nextInt(45));
+            }else{
+                while(!grille3.contains(r)){
+                    r.nextInt(45);
+                    grille3.add(r.nextInt(45));
+                }
+            }
             r.nextInt(45);
-            //if(r not in grille4)
-            grille4[i] =  r.nextInt(45);
+            if(!grille4.contains(r)){
+                grille4.add(r.nextInt(45));
+            }else{
+                while(!grille4.contains(r)){
+                    r.nextInt(45);
+                    grille4.add(r.nextInt(45));
+                }
+            }
             r.nextInt(45);
-            //if(r not in grille5)
-            grille5[i] =  r.nextInt(45);
+            if(!grille5.contains(r)){
+                grille5.add(r.nextInt(45));
+            }else{
+                while(!grille5.contains(r)){
+                    r.nextInt(45);
+                    grille5.add(r.nextInt(45));
+                }
+            }
         }
         //création des cinqs grille de révelation 
         for (int i=0; i<9; i++){
@@ -46,77 +77,55 @@ public class bingo {
         System.out.println("Le jeu commence ! ");
         chiffre = (int)r.nextInt(45);
         affichageDé(chiffre);
-
+        
     }
 
-    String[][] affichage_grille(int[] grille){
-        String[][] grille_afficher = new String[6][6];
-        grille_afficher[0][0] = "--";
-        grille_afficher[0][1] = "--";
-        grille_afficher[0][2] = "--";
-        grille_afficher[0][3] = "--";
-        grille_afficher[0][4] = "--";
-        grille_afficher[0][5] = "--";
-        grille_afficher[0][6] = "--";
-        grille_afficher[6][0] = "--";
-        grille_afficher[6][1] = "--";
-        grille_afficher[6][2] = "--";
-        grille_afficher[6][3] = "--";
-        grille_afficher[6][4] = "--";
-        grille_afficher[6][5] = "--";
-        grille_afficher[6][6] = "--";
-        grille_afficher[1][0] = "|";
-        grille_afficher[2][0] = "|";
-        grille_afficher[3][0] = "|";
-        grille_afficher[4][0] = "|";
-        grille_afficher[5][0] = "|";
-        grille_afficher[6][0] = "--";
-        grille_afficher[1][1] = grille[0] + "";
-        grille_afficher[2][1] = "--";
-        grille_afficher[3][1] = grille[3] + "";
-        grille_afficher[4][1] = "--";
-        grille_afficher[5][1] = grille[6] + "";
-        grille_afficher[6][1] = "--";
-        grille_afficher[1][2] = "|";
-        grille_afficher[2][2] = "|";
-        grille_afficher[3][2] = "|";
-        grille_afficher[4][2] = "|";
-        grille_afficher[5][2] = "|";
-        grille_afficher[6][2] = "|";
-        grille_afficher[1][3] = grille[1] + "";
-        grille_afficher[2][3] = "--";
-        grille_afficher[3][3] = grille[4] + "";
-        grille_afficher[4][3] = "--";
-        grille_afficher[5][3] = grille[7] + "";
-        grille_afficher[6][3] = "--";
-        grille_afficher[1][4] = "|";
-        grille_afficher[2][4] = "|";
-        grille_afficher[3][4] = "|";
-        grille_afficher[4][4] = "|";
-        grille_afficher[5][4] = "|";
-        grille_afficher[6][4] = "|";
-        grille_afficher[1][5] = grille[2] + "";
-        grille_afficher[2][5] = "--";
-        grille_afficher[3][5] = grille[5] + "";
-        grille_afficher[4][5] = "--";
-        grille_afficher[5][5] = grille[8] + "";
-        return grille_afficher;
+    static void affichage_grille(ArrayList grille){
+        for (int i=0; i<8; i++){
+            System.out.print((" - "));
+        }
+        System.out.println();
+        int y = 0;
+        for (int x=0; x<8;x++){
+            for (int i=0; i<8; i++){
+                if(i%2 == 0){
+                    System.out.print(" | ");
+                }else{
+                    System.out.print(grille.get(y) + "");
+                    y++;
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+        for (int i=0; i<7; i++){
+            System.out.print(("--"));
+        }
     }
 
-    String[][] affichageDé(int chiffre){
-        String[][] chiffre_afficher = new String[3][3];
-        chiffre_afficher[0][0]= "--";
-        chiffre_afficher[0][1]= "--";
-        chiffre_afficher[0][2]= "--";
-        chiffre_afficher[2][0]= "--";
-        chiffre_afficher[2][1]= "--";
-        chiffre_afficher[2][2]= "--";
-        chiffre_afficher[0][1]= "|";
-        chiffre_afficher[0][2]= "|";
-        chiffre_afficher[2][1]= "|";
-        chiffre_afficher[2][2]= "|";
-        chiffre_afficher[1][1]= chiffre + "";
-        return chiffre_afficher;
+    static void affichageDé(int chiffre){
+        for (int i=0; i<3; i++){
+            System.out.print((" -"));
+        }
+        System.out.println();
+        System.out.println(" | " + chiffre + " |");
+        for (int i=0; i<3; i++){
+            System.out.print((" -"));
+        }
     }
     
+    public static void main(String[] args) {
+        affichageDé(1);
+        ArrayList a = new ArrayList<>();
+        a.add(45);
+        a.add(22);
+        a.add(44);
+        a.add(3);
+        a.add(25);
+        a.add(18);
+        a.add(5);
+        a.add(4);
+        a.add(10);
+        affichage_grille(a);
+    }
 }
