1894591272f19ec6470043cdb8c5dae3fd0f1998
nathan desmee <nathan.desmee.etu@univ-lille.fr>
2023-09-04 16:44:34 +0200

    changement structure

---
+++ src/main/App.java
@@ -0,0 +1,32 @@
+package main;
+import java.io.File;
+import java.util.Scanner;
+
+public class App {
+    public Scanner scanner = new Scanner(System.in);
+    private Joueur joueur;
+
+    private void introduction(){
+        clear();
+        System.out.print("Bonjour veillez entrer votre nom pour jouer : ");
+        String nom = scanner.next();
+        File file = new File(System.getProperty("user.dir") + File.separator + "res" + File.separator + nom + ".csv");
+        if(!file.exists()){
+            clear();
+            System.out.println("Ahh un nouveau joueur soit le bienvenu !!");
+            joueur = new Joueur(nom, 100, 100, 100);
+        }
+    }
+
+    public static void clear()
+	{
+        final String ESC = "\033[";
+        System.out.print (ESC + "2J");
+        System.out.print (ESC + "0;0H");
+        System.out.flush();
+	}
+
+    public static void main(String[] args) throws Exception {
+        
+    }
+}
---
+++ src/main/Joueur.java
@@ -0,0 +1,16 @@
+package main;
+public class Joueur {
+    private String nom;
+    private int vie;
+    private int argent;
+    private int bonheur;
+
+    public Joueur(String nom, int vie, int argent, int bonheur) {
+        this.nom = nom;
+        this.vie = vie;
+        this.argent = argent;
+        this.bonheur = bonheur;
+    }
+
+    
+}
