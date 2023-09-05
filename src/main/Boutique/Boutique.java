6f37f321fa6744ab107e6d565f6de1369f2d6283
Quentin Moutté <q.moutte@icloud.com>
2023-09-05 15:30:25 +0200

    debut boutique

--- src/main/Boutique/Boutique.java
+++ src/main/Boutique/Boutique.java
@@ -1,5 +1,41 @@
 package main.Boutique;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import main.Consommable.*;
+
 public class Boutique {
     
+    private List<Consommable> consommables;
+
+    public Boutique(){
+        this.consommables = new ArrayList<Consommable>();
+    }
+
+    public static void main(String[] args){
+        Boutique b = new Boutique();
+        b.consommables.add(new Nourriture(5,8,"pizza"));
+        b.consommables.add(new Nourriture(8,6,"kebab"));
+        b.consommables.add(new Nourriture(10,15,"tacos"));
+        b.consommables.add(new Nourriture(15,12,"sushi"));
+        b.consommables.add(new Nourriture(20,17,"raclette"));
+
+        b.consommables.add(new Bonheur(10,5,"vetement"));
+        b.consommables.add(new Bonheur(15,100,"voiture"));
+        b.consommables.add(new Bonheur(20,1000,"maison"));
+        b.consommables.add(new Bonheur(75,10000,"femme"));
+        b.consommables.add(new Bonheur(50,7000,"enfant"));
+
+        for(Consommable c: b.consommables){
+            b.afficher(c);
+        }
+    }
+
+    public void afficher(Consommable c){
+        System.out.println(c.effet());
+        // if(c instanceof Bonheur){
+        //     System.out.println(c.getNom() + " pour " + c.getPrix() + " vous donneras ");
+        // }
+    }
 }
--- src/main/Consommable/Consommable.java
+++ src/main/Consommable/Consommable.java
@@ -3,8 +3,30 @@ package main.Consommable;
 public class Consommable {
     
     private int prix;
+    private String nom;
 
-    public Consommable(int prix){
+    public Consommable(int prix, String nom){
         this.prix = prix;
+        this.nom = nom;
+    }
+
+    public String effet(){
+        return "ceci est un consommable";
+    }
+
+    public int getPrix() {
+        return prix;
+    }
+
+    public void setPrix(int prix) {
+        this.prix = prix;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
     }
 }
